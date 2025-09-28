f2aeb811cd9530a44d89e5508b2e4d9deb06912c
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-09-28 22:59:40 +0200

    Initial Commit

---
+++ SampleAsyncAwaitPattern/SampleAsyncAwaitPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAsyncAwaitPatternApp.swift
+//  SampleAsyncAwaitPattern
+//
+//  Created by Azizbek Asadov on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SampleAsyncAwaitPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
